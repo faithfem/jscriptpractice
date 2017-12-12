51576f5918520271087b7ee233838156e3e6ddcd
Faith <fmugure@intracitygeeks.org>
2017-12-12 01:19:26 +0000

    train

---
+++ train.js
@@ -0,0 +1,10 @@
+//FOR LOOPS CODE ACADEMY//
+
+var totalTrains = 12;
+var trainsOperational = 8;
+
+var trainNumber = 1;
+while(trainNumber <=trainsOperational){
+console.log("Train#" + trainNumber + " is running.");
+trainNumber++;
+}
