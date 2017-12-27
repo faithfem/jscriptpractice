ea51430eef50c3c9a0d89f8f80aefcf8d1d2d105
Faith <fmugure@intracitygeeks.org>
2017-12-27 03:23:48 +0000

    functions

---
+++ functions.js
@@ -0,0 +1,7 @@
+function getSize (width, height, depth){
+    var area = width * height;
+    var volume = width * height * depth;
+}
+
+var johnAreaLand = getSize(3,2,3);
+var johnJarVolume = getSize(3,2,3);
