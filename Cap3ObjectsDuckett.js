3c9ae828f2d8424ac1ef73e0c976d21103408c65
Faith <fmugure@intracitygeeks.org>
2017-12-28 12:16:55 +0000

    String Objects

---
+++ Cap3ObjectsDuckett.js
@@ -0,0 +1,6 @@
+var saying = "Home Sweet Home";
+
+console.log(saying.length);
+console.log(saying.toUpperCase());
+console.log(saying.toLowerCase());
+console.log(saying.charAt(8));
--- functions.js
+++ functions.js
@@ -15,12 +15,15 @@ console.log(volumeOne);
 console.log(perimeter);
 
 // sometimes you can have the function inside a variable, so when you call the variable, it runs the function
+// below, i can re-use variable names due to scope
 
 var area = function(w, l){      //function declared by variable
-    return w * l;
+    return w * l;               //return belongs to functions and you'll get an error if you use outside of that
 };
 
 var size = area(7,4);         
-console.log(size);
+console.log(size); //PS: Remember, console.log is used in test mode, not in production. In production, it's usually document.write
+
+
 
 
