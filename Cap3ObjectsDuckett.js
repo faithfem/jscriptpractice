2fe034f728b5ddee87ebbfce3e43b5e3615d263a
Faith <fmugure@intracitygeeks.org>
2017-12-28 12:25:57 +0000

    String Objects

--- Cap3ObjectsDuckett.js
+++ Cap3ObjectsDuckett.js
@@ -1,6 +1,13 @@
-var saying = "Home Sweet Home";
+var saying = "Home Sweet Home ";
 
 console.log(saying.length);
 console.log(saying.toUpperCase());
 console.log(saying.toLowerCase());
 console.log(saying.charAt(8));
+console.log(saying.indexOf("ee"));      //position of ee    
+console.log(saying.lastIndexOf("e"));    //position of the last e
+console.log(saying.substring(8,14));
+console.log(saying.substr(8,14));
+console.log(saying.split(""));          //prints vertically
+console.log(saying.trim());
+console.log(saying.replace("me","w")); //only replaces the first one it finds
