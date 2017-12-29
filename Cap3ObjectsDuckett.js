e14848c38f0edc52b57406a837ef4d1f494d5e5d
Faith <fmugure@intracitygeeks.org>
2017-12-29 11:15:06 +0000

    Cap3ObjectsNumbers

--- Cap3ObjectsDuckett.js
+++ Cap3ObjectsDuckett.js
@@ -8,6 +8,7 @@ console.log(saying.indexOf("ee"));      //position of ee
 console.log(saying.lastIndexOf("e"));    //position of the last e
 console.log(saying.substring(8,14));
 console.log(saying.substr(8,14));
-console.log(saying.split(""));          //prints vertically
+console.log(saying.split(""));          //splits after each letter (vertical display)
+console.log(saying.split(" "));         //splits after each string/word
 console.log(saying.trim());
 console.log(saying.replace("me","w")); //only replaces the first one it finds
